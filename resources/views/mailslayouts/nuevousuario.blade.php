7f79507368e529f5e997517411f01f53aa865997
SamuelAmbrosio99 <samuelambrosio99@gmail.com>
2021-08-19 15:36:40 -0600

    new user mail template

--- resources/views/mailslayouts/nuevousuario.blade.php
+++ resources/views/mailslayouts/nuevousuario.blade.php
@@ -1,4 +1,375 @@
-<h1>Se ha creado un nuevo usuario en la plataforma de funerarias.</h1>
-<br>
-<h2><b>Su contraseña es: </b><span>{{$password}}</span></h2>
-<h3>Ingrese en {{url('/')}}</h3>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+	<!--[if gte mso 9]><xml><o:OfficeDocumentSettings><o:AllowPNG/><o:PixelsPerInch>96</o:PixelsPerInch></o:OfficeDocumentSettings></xml><![endif]-->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width">
+	<!--[if !mso]><!-->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!--<![endif]-->
+	<title></title>
+	<!--[if !mso]><!-->
+	<!--<![endif]-->
+	<style type="text/css">
+		body {
+			margin: 0;
+			padding: 0;
+		}
+
+		table,
+		td,
+		tr {
+			vertical-align: top;
+			border-collapse: collapse;
+		}
+
+		* {
+			line-height: inherit;
+		}
+
+		a[x-apple-data-detectors=true] {
+			color: inherit !important;
+			text-decoration: none !important;
+		}
+
+
+	</style>
+	<style type="text/css" id="media-query">
+		@media (max-width: 660px) {
+
+			.block-grid,
+			.col {
+				min-width: 320px !important;
+				max-width: 100% !important;
+				display: block !important;
+			}
+
+			.block-grid {
+				width: 100% !important;
+			}
+
+			.col {
+				width: 100% !important;
+			}
+
+			.col_cont {
+				margin: 0 auto;
+			}
+
+			img.fullwidth,
+			img.fullwidthOnMobile {
+				width: 100% !important;
+			}
+
+			.no-stack .col {
+				min-width: 0 !important;
+				display: table-cell !important;
+			}
+
+			.no-stack.two-up .col {
+				width: 50% !important;
+			}
+
+			.no-stack .col.num2 {
+				width: 16.6% !important;
+			}
+
+			.no-stack .col.num3 {
+				width: 25% !important;
+			}
+
+			.no-stack .col.num4 {
+				width: 33% !important;
+			}
+
+			.no-stack .col.num5 {
+				width: 41.6% !important;
+			}
+
+			.no-stack .col.num6 {
+				width: 50% !important;
+			}
+
+			.no-stack .col.num7 {
+				width: 58.3% !important;
+			}
+
+			.no-stack .col.num8 {
+				width: 66.6% !important;
+			}
+
+			.no-stack .col.num9 {
+				width: 75% !important;
+			}
+
+			.no-stack .col.num10 {
+				width: 83.3% !important;
+			}
+
+			.video-block {
+				max-width: none !important;
+			}
+
+			.mobile_hide {
+				min-height: 0px;
+				max-height: 0px;
+				max-width: 0px;
+				display: none;
+				overflow: hidden;
+				font-size: 0px;
+			}
+
+			.desktop_hide {
+				display: block !important;
+				max-height: none !important;
+			}
+
+			.img-container.big img {
+				width: auto !important;
+			}
+            .es-button-border {
+                border-style: solid solid solid solid;
+                border-color: #2CB543 #2CB543 #2CB543 #2CB543;
+                background: #5C68E2;
+                border-width: 0px 0px 0px 0px;
+                display: inline-block;
+                border-radius: 0px;
+                width: auto;
+            }
+
+            
+		}
+	</style>
+</head>
+
+<body class="clean-body" style="margin: 0; padding: 0; -webkit-text-size-adjust: 100%; background-color: #FFFFFF;">
+	<!--[if IE]><div class="ie-browser"><![endif]-->
+	<table class="nl-container" style="table-layout: fixed; vertical-align: top; min-width: 320px; border-spacing: 0; border-collapse: collapse; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #FFFFFF; width: 100%;" cellpadding="0" cellspacing="0" role="presentation" width="100%" bgcolor="#FFFFFF" valign="top">
+		<tbody>
+			<tr style="vertical-align: top;" valign="top">
+				<td style="word-break: break-word; vertical-align: top;" valign="top">
+					<!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td align="center" style="background-color:#FFFFFF"><![endif]-->
+					<div style="background-color:transparent;">
+						<div class="block-grid " style="min-width: 320px; max-width: 640px; overflow-wrap: break-word; word-wrap: break-word; word-break: break-word; Margin: 0 auto; background-color: transparent;">
+							<div style="border-collapse: collapse;display: table;width: 100%;background-color:transparent;">
+								<!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0" style="background-color:transparent;"><tr><td align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:640px"><tr class="layout-full-width" style="background-color:transparent"><![endif]-->
+								<!--[if (mso)|(IE)]><td align="center" width="640" style="background-color:transparent;width:640px; border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent;" valign="top"><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 0px; padding-left: 0px; padding-top:5px; padding-bottom:5px;"><![endif]-->
+								<div class="col num12" style="min-width: 320px; max-width: 640px; display: table-cell; vertical-align: top; width: 640px;">
+									<div class="col_cont" style="width:100% !important;">
+										<!--[if (!mso)&(!IE)]><!-->
+										<div style="border-top:0px solid transparent; border-left:0px solid transparent; border-bottom:0px solid transparent; border-right:0px solid transparent; padding-top:5px; padding-bottom:5px; padding-right: 0px; padding-left: 0px;">
+											<!--<![endif]-->
+											<div class="img-container left fixedwidth" align="left" style="padding-right: 0px;padding-left: 0px;">
+												<!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr style="line-height:0px"><td style="padding-right: 0px;padding-left: 0px;" align="left"><![endif]-->
+												<!--[if mso]></td></tr></table><![endif]-->
+											</div>
+											<div style="font-size:16px;text-align:center;font-family:Arial, Helvetica Neue, Helvetica, sans-serif">
+												<div class="es-wrapper-color">
+
+													<table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0">
+														<tr>
+															<td valign="top">
+																<table cellpadding="0" cellspacing="0" class="es-header" align="center">
+																	<tr>
+																		<td align="center">
+																			<table bgcolor="#ffffff" class="es-header-body" align="center" cellpadding="0" cellspacing="0" width="600">
+																				<tr>
+																					<td class="es-p10t es-p10b es-p20r es-p20l" align="left">
+																						<table cellpadding="0" cellspacing="0" width="100%">
+																							<tr>
+																								<td width="560" class="es-m-p0r" valign="top" align="center">
+																									<table cellpadding="0" cellspacing="0" width="100%">
+																										<tr>
+																											<td align="center" class="es-p20b" style="font-size: 0px">
+
+																											</td>
+																										</tr>
+																									</table>
+																								</td>
+																							</tr>
+																						</table>
+																					</td>
+																				</tr>
+																			</table>
+																		</td>
+																	</tr>
+																</table>
+																<table cellpadding="0" cellspacing="0" class="es-content" align="center">
+																	<tr>
+																		<td align="center">
+																			<table bgcolor="#ffffff" class="es-content-body" align="center" cellpadding="0" cellspacing="0" width="600">
+																				<tr>
+																					<td class="es-p30t es-p10b es-p20r es-p20l" align="left">
+																						<table cellpadding="0" cellspacing="0" width="100%">
+																							<tr>
+																								<td width="560" align="center" valign="top">
+																									<table cellpadding="0" cellspacing="0" width="100%">
+																										<tr>
+																											<td align="justify" class="es-p5t es-p5b es-p30r es-p30l ">
+                                                                                                                <nav style="margin: 50px 0px 0px 0px;padding: 3px;background: #A2F0D3!important;color:white!important; text-align: center;">
+                                                                                                                    <h1 style="font-size: 28px;line-height: 28px;color: #008080">
+                                                                                                                        SISTEMA </br> DE SERVICIOS FUNERARIOS
+                                                                                                                    </h1>
+                                                                                                                </nav>
+																											</td>
+																										</tr>
+																										<tr>
+																											<td align="center" class="es-p10t es-p10b" style="font-size: 0px">
+
+
+																											</td>
+																										</tr>
+																										<tr>
+																											<td align="center" class="es-p10b">
+																												<h1 style="font-size: 28px;line-height: 28px;color: #008080">
+																													¡Su cuenta ha sido<br />creada con éxito!</h1>
+																											</td>
+																										</tr>
+																										<tr>
+																											<td align="center" class="es-p5t es-p5b es-p40r es-p40l es-m-p0r es-m-p0l">
+																												<p style="font-size: 17px">Puede ingresar haciendo
+																													click en el siguiente link:<br />
+                                                                                                                    <span style="color:#0000CD">
+                                                                                                                        <a href="{{url('/')}}" target="_blank">{{url('/')}}</a>
+                                                                                                                    </span>
+																												</p>
+																											</td>
+																										</tr>
+																									</table>
+																								</td>
+																							</tr>
+																						</table>
+																					</td>
+																				</tr>
+																				<tr>
+																					<td class="es-p10t es-p10b es-p20r es-p20l" align="left">
+																						<table cellpadding="0" cellspacing="0" width="100%">
+																							<tr>
+																								<td width="560" align="center" valign="top">
+																									<table cellpadding="0" cellspacing="0" width="100%" style="border-left:2px dashed #cccccc;border-right:2px dashed #cccccc;border-top:2px dashed #cccccc;border-bottom:2px dashed #cccccc;border-radius: 5px;border-collapse: separate">
+																										<tr>
+																											<td align="center" class="es-p20t es-p20r es-p20l">
+																												<h2 style="font-size: 18px">Su contraseña es:</h2>
+																											</td>
+																										</tr>
+																										<tr>
+																											<td align="center" class="es-p10t es-p20b es-p20r es-p20l">
+																												<h1 style="color: #0000cd;font-size: 30px">
+																													<strong>{{$password}}</strong>
+																												</h1>
+																											</td>
+																										</tr>
+																									</table>
+																								</td>
+																							</tr>
+																						</table>
+																					</td>
+																				</tr>
+																				<tr>
+																					<td class="es-p30b es-p20r es-p20l" align="left">
+																						<table cellpadding="0" cellspacing="0" width="100%">
+																							<tr>
+																								<td width="560" align="center" valign="top">
+																									<table cellpadding="0" cellspacing="0" width="100%" style="border-radius: 5px;border-collapse: separate">
+																										<tr>
+																											<td align="center" class="es-p10t es-p10b"><span class="es-button-border" style="border-radius: 6px;background: #45818e">
+                                                                                                                <a href class="es-button" target="_blank" style="border-left-width: 30px;border-right-width: 30px;border-radius: 6px;background: #45818e;border-color: #45818e">INGRESAR</a>
+                                                                                                            </span>
+																											</td>
+																										</tr>
+																									</table>
+																								</td>
+																							</tr>
+																						</table>
+																					</td>
+																				</tr>
+																			</table>
+																		</td>
+																	</tr>
+																</table>
+																<table cellpadding="0" cellspacing="0" class="es-footer" align="center">
+																	<tr>
+																		<td align="center">
+																			<table class="es-footer-body" align="center" cellpadding="0" cellspacing="0" width="640" style="background-color: transparent">
+																				<tr>
+																					<td class="es-p20t es-p20b es-p20r es-p20l" align="left">
+																						<table cellpadding="0" cellspacing="0" width="100%">
+																							<tr>
+																								<td width="600" align="left">
+																									<table cellpadding="0" cellspacing="0" width="100%">
+																										<tr>
+																											<td align="center" style="font-size: 0px">
+
+																											</td>
+																										</tr>
+																										<tr>
+																											<td align="center" class="es-p35b">
+																												<p>Urgencias Médicas © 2021 Balance Psicología,
+																													Todos los derechos reservados.</p>
+																												<p>Guatemala, C.A.</p>
+																											</td>
+																										</tr>
+																									</table>
+																								</td>
+																							</tr>
+																						</table>
+																					</td>
+																				</tr>
+																			</table>
+																		</td>
+																	</tr>
+																</table>
+																<table cellpadding="0" cellspacing="0" class="es-content" align="center">
+																	<tr>
+																		<td class="es-info-area" align="center">
+																			<table class="es-content-body" align="center" cellpadding="0" cellspacing="0" width="600" style="background-color: transparent" bgcolor="rgba(0, 0, 0, 0)">
+																				<tr>
+																					<td class="es-p20" align="left">
+																						<table cellpadding="0" cellspacing="0" width="100%">
+																							<tr>
+																								<td width="560" align="center" valign="top">
+																									<table cellpadding="0" cellspacing="0" width="100%">
+																										<tr>
+																											<td align="center" class="es-infoblock">
+																												<p></p>
+																											</td>
+																										</tr>
+																									</table>
+																								</td>
+																							</tr>
+																						</table>
+																					</td>
+																				</tr>
+																			</table>
+																		</td>
+																	</tr>
+																</table>
+															</td>
+														</tr>
+													</table>
+												</div>
+											</div>
+											<div class="img-container center fixedwidth" align="center" style="padding-right: 0px;padding-left: 0px;">
+												<!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr style="line-height:0px"><td style="padding-right: 0px;padding-left: 0px;" align="center"><![endif]--><img class="center fixedwidth" align="center" border="0" src="https://d15k2d11r6t6rl.cloudfront.net/public/users/Integrators/BeeProAgency/698669_681347/editor_images/44841627340517851_2.png" style="text-decoration: none; -ms-interpolation-mode: bicubic; height: auto; border: 0; width: 320px; max-width: 100%; display: block;" width="320">
+												<!--[if mso]></td></tr></table><![endif]-->
+											</div>
+											<!--[if (!mso)&(!IE)]><!-->
+										</div>
+										<!--<![endif]-->
+									</div>
+								</div>
+								<!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+								<!--[if (mso)|(IE)]></td></tr></table></td></tr></table><![endif]-->
+							</div>
+						</div>
+					</div>
+					<!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<!--[if (IE)]></div><![endif]-->
+</body>
+
+</html>
