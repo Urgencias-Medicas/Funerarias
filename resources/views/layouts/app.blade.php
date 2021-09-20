fd3eed2a2660cac75babe90eae4cbd64d81350c9
SamuelAmbrosio99 <samuelambrosio99@gmail.com>
2021-09-20 13:35:12 -0600

    messenger chat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,34 @@
 </head>
 
 <body style="background: linear-gradient(90deg, rgba(172, 182, 229, 0.25) 0%, rgba(134, 253, 232, 0.25) 100%);">
+
     <div id="app">
+    <div id="fb-root"></div>
+
+<!-- Your plugin de chat code -->
+<div id="fb-customer-chat" class="fb-customerchat">
+</div>
+
+<script>
+  var chatbox = document.getElementById('fb-customer-chat');
+  chatbox.setAttribute("page_id", "113183360855355");
+  chatbox.setAttribute("attribution", "biz_inbox");
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v12.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
         <nav class="navbar-expand-md navbar navbar-dark bg-dark shadow-sm" style="background: #45B5A1!important;color:white!important;">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
